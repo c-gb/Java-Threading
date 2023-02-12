254783e483c5fc9a73791afb5f78282e5bb4a9d3
simplifiedlearningblog <102782876+c-gb@users.noreply.github.com>
2023-02-12 19:02:05 +0530

    Added Daemon thread priority example

--- src/com/slb/thread/DaemonThreadExample.java
+++ src/com/slb/thread/DaemonThreadExample.java
@@ -7,22 +7,33 @@ public class DaemonThreadExample extends Thread{
 
         System.out.println("running...");
 
-        System.out.println(this.getName());
-        System.out.println("is this daemon thread ? :" + this.isDaemon());
+//        System.out.println(this.getName());
+//        System.out.println("is this daemon thread ? :" + this.isDaemon());
+
+        System.out.println("PRIORITY OF "+this.getName() +" THREAD : " + this.getPriority());
     }
 
     //will first create one user thread and then will create another one for daemon
     public static void main(String[] args) {
-        DaemonThreadExample userThread = new DaemonThreadExample();
+        DaemonThreadExample userThread1 = new DaemonThreadExample();
         DaemonThreadExample daemonThread = new DaemonThreadExample();
+        DaemonThreadExample userThread2 = new DaemonThreadExample();
+        DaemonThreadExample userThread3 = new DaemonThreadExample();
+        DaemonThreadExample daemonThread2 = new DaemonThreadExample();
 
         daemonThread.setDaemon(true);
+        daemonThread2.setDaemon(true);
 
-        userThread.setName("USER THREAD");
-        daemonThread.setName("DAEMON THREAD");
+        userThread1.setName("USER THREAD-1");
+        userThread2.setName("USER THREAD-2");
+        userThread3.setName("USER THREAD-3");
+        daemonThread.setName("DAEMON THREAD 1 ");
+        daemonThread2.setName("DAEMON THREAD 2 ");
 
-        userThread.run();
-        daemonThread.run();
+        userThread1.start();
+        daemonThread.start();
+        userThread2.start();
+        userThread3.start();
 
         System.out.println("checking main thread is daemon or not");
         System.out.println(Thread.currentThread().isDaemon());
